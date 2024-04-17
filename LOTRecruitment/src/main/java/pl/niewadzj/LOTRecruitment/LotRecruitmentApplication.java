cddc9f284bc4caa406482045ab74f378f442c58e
JakubNiewadzi <01169600@pw.edu.pl>
2024-04-17 12:39:11 +0200

    project init

---
+++ LOTRecruitment/src/main/java/pl/niewadzj/LOTRecruitment/LotRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package pl.niewadzj.LOTRecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotRecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LotRecruitmentApplication.class, args);
+	}
+
+}
---
+++ LOTRecruitment/src/test/java/pl/niewadzj/LOTRecruitment/LotRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.niewadzj.LOTRecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotRecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
